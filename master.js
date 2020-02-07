60ac4541d0763a0983306d16f235201ca692bd82
atomazos <at2276@columbia.edu>
2020-02-07 17:14:40 -0600

    edits to master files

--- master.js
+++ master.js
@@ -5,19 +5,19 @@ innerRadius = outerRadius - 24;
  
 var formatPercent = d3.format(".1%");
  
-var arc = d3.svg.arc()
+var arc = d3.arc()
 .innerRadius(innerRadius)
 .outerRadius(outerRadius);
  
-var layout = d3.layout.chord()
-.padding(.04)
+var layout = d3.chord()
+.padAngle(0.04)
 .sortSubgroups(d3.descending)
 .sortChords(d3.ascending);
- 
-var path = d3.svg.chord()
+
+var path = d3.ribbon()
 .radius(innerRadius);
  
-var svg = d3.select("body").append("svg")
+var svg = d3.select("viz").append("svg")
 .attr("width", width)
 .attr("height", height)
 .append("g")
@@ -26,24 +26,30 @@ var svg = d3.select("body").append("svg")
 
 svg.append("circle")
 .attr("r", outerRadius);
-d3.csv("areas.csv", function(areas) {
-d3.json("matrix.json", function(matrix) {
- console.log(areas)
- console.log(matrix)
+
+d3.csv("areas.csv").then(function(areas) {
+    console.log(areas[0]);
+  });
+
+// d3.csv("areas.csv", function(areas) {
+d3.json("matrix.json").then(function(matrix) {
+    console.log(matrix)
 // Compute the chord layout.
-layout.matrix(matrix);
+// layout.matrix(matrix);
  
+
 // Add a group per neighborhood.
 var group = svg.selectAll(".group")
-.data(layout.groups)
+// .data(layout.groups)
+.datum(layout(matrix))
 .enter().append("g")
 .attr("class", "group")
 .on("mouseover", mouseover);
  
 // Add a mouseover title.
-// group.append("title").text(function(d, i) {
-// return areas[i].name + ": " + formatPercent(d.value) + " of origins";
-// });
+group.append("title").text(function(d, i) {
+return areas[i].name + ": " + formatPercent(d.value) + " of origins";
+});
  
 // Add the group arc.
 var groupPath = group.append("path")
@@ -66,7 +72,8 @@ groupText.filter(function(d, i) { return groupPath[0][i].getTotalLength() / 2 - 
  
 // Add the chords.
 var chord = svg.selectAll(".chord")
-.data(layout.chords)
+// .data(layout.chords)
+.datum(layout(matrix))
 .enter().append("path")
 .attr("class", "chord")
 .style("fill", function(d) { return areas[d.source.index].color; })
@@ -89,4 +96,4 @@ return p.source.index != i
 });
 }
 });
-});
+// });
